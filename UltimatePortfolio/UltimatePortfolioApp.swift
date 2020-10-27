5065efad057e14d5c7542e7f2489a71027ee6d48
Igor Malyarov <igor1309@gmail.com>
2020-10-27 10:09:23 +0300

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Igor Malyarov on 27.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
